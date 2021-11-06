f5c8f941eb68df16889e33cd6dbc6a39980bb990
cameronrowe1 <cameronrowe1@gmail.com>
2021-11-05 22:17:21 -0700

    added api province controller

---
+++ Controllers/Provinces1Controller.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab_04.Data;
+using Lab_04.Models;
+
+namespace lab_04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Provinces1Controller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Provinces1Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Provinces1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Province>>> GetProvinces()
+        {
+            return await _context.Provinces.ToListAsync();
+        }
+
+        // GET: api/Provinces1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Province>> GetProvince(string id)
+        {
+            var province = await _context.Provinces.FindAsync(id);
+
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return province;
+        }
+
+        // PUT: api/Provinces1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProvince(string id, Province province)
+        {
+            if (id != province.ProvinceCode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(province).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvinceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Provinces1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Province>> PostProvince(Province province)
+        {
+            _context.Provinces.Add(province);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProvinceExists(province.ProvinceCode))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProvince", new { id = province.ProvinceCode }, province);
+        }
+
+        // DELETE: api/Provinces1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProvince(string id)
+        {
+            var province = await _context.Provinces.FindAsync(id);
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            _context.Provinces.Remove(province);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProvinceExists(string id)
+        {
+            return _context.Provinces.Any(e => e.ProvinceCode == id);
+        }
+    }
+}
